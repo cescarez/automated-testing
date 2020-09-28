c8310617df6a50240738c255e89ca5a84736727c
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-28 15:15:29 -0700

    Added instance variable and helper method

--- lib/card.rb
+++ lib/card.rb
@@ -9,20 +9,25 @@ class Card
   def initialize(value, suit)
     @value = NUMS.include?(value) ? value : (raise ArgumentError, "Invalid value entered for Card.")
     @suit = SUITS.include?(suit) ? suit : (raise ArgumentError, "Invalid suit entered for Card.")
+    @number = human_readable(value)
   end
 
   def to_s
-    case @value
+    return "#{@number} of #{@suit.to_s}"
+  end
+
+  def human_readable(value)
+    case value
     when 1
-      return "Ace of #{suit.to_s}"
+      @number = "Ace"
     when 2..10
-      return "#{value} of #{suit.to_s}"
+      @number = value
     when 11
-      return "Jack of #{suit.to_s}"
+      @number = "Jack"
     when 12
-      return "Queen of #{suit.to_s}"
+      @number = "Queen"
     when 13
-      return "King of #{suit.to_s}"
+      @number = "King"
     end
   end
 
