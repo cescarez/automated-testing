4209aa1674e056324443a3a58835d7d4b13cb7ff
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-28 13:46:34 -0700

    card instantiation written, tests passing

--- lib/card.rb
+++ lib/card.rb
@@ -3,12 +3,12 @@
 
 class Card
   attr_reader :value, :suit
+  NUMS = (1..13).to_a
+  SUITS = [:hearts, :spades, :clubs, :diamonds]
 
   def initialize(value, suit)
-    @value = value
-    @suit = suit
-    
-
+    @value = NUMS.include?(value) ? value : (raise ArgumentError, "Invalid value entered for Card.")
+    @suit = SUITS.include?(suit) ? suit : (raise ArgumentError, "Invalid suit entered for Card.")
   end
 
   def to_s
