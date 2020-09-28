2aaced340981568381d4f62d32399c3d1fb1a99a
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-28 15:04:43 -0700

    Deck constructor and tests written

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,9 +4,19 @@
 require_relative 'card'
 
 class Deck
+  attr_reader :cards
 
   def initialize
+    @cards = generate_all_cards
+  end
 
+  def generate_all_cards
+    all_cards = Card::NUMS.flat_map do |value|
+      Card::SUITS.flat_map do |suit|
+        Card.new(value, suit)
+      end
+    end
+    return all_cards
   end
 
   def draw
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,5 +5,23 @@ describe Deck do
   # What should you test?
     # 1.  Expected Behavior
     # 2.  Edge cases
+  describe "You can create a Card instance" do
 
+    it "can be created" do
+      expect(Deck.new).must_be_instance_of Deck
+    end
+
+    it "deck has correct number and type of objects" do
+      expect(Deck.new.cards.count).must_equal 52
+      expect(Deck.new.cards[0]).must_be_instance_of Card
+      expect(Deck.new.cards[-1]).must_be_instance_of Card
+    end
+
+    it "deck is created with cards that have correct value and suit" do
+      expect(Deck.new.cards[0].value).must_equal 1
+      expect(Deck.new.cards[0].suit).must_equal :spades
+      expect(Deck.new.cards[-1].value).must_equal 13
+      expect(Deck.new.cards[-1].suit).must_equal :hearts
+    end
+  end
 end
